66ddc90b943f55cbe5139f4dbe8e7d662975490b
BT_1E10_20HY <haleyy3@nycstudents.net>
2022-02-04 11:26:34 -0500

    Commmited

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is the project before sharing and deleting");
+        System.out.println("This is the project after sharing and deleting");
     }
 }
